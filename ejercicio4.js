e75a4439968b75e999b06815779c0b5709a711de
damian <jiselsantos18@gmail.com>
2024-09-18 14:43:55 -0600

    Add files via upload

---
+++ ejercicio4.js
@@ -0,0 +1,11 @@
+//Determinar si una persona es mayor o menor de edad
+let e = parseInt(prompt("Ingrese su edad"));
+if (e >= 18){
+    document.write("Usted es mayor de edad");
+}
+else if(e <= 17){
+    document.write("Usted es menor de edad");
+}
+else {
+
+}
